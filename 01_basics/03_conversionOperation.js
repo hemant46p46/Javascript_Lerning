692943686bde1983a5b960a083e82ff149072062
Hemant Patel <hemantp071999@gmail.com>
2024-12-05 08:52:59 +0530

    Operations and expressions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,3 +43,46 @@ let stringNumber = String(someNumber);
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ***************************** Operation *****************************
+
+let value = 3
+let negValue = -value;
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello";
+let str2 = " world";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "3");//33
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+// console.log(true+);// wrong expression
+console.log(+"");
+
+let n1, n2, n3;
+n1 = n2 = n3 = 2 + 2;
+
+let gameCounter = 100;
+console.log(++gameCounter);
+console.log(gameCounter++);
