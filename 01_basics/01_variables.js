f4c8e410f1632f9e6aa1850e3090058201d117cb
Hemant Patel <hemantp071999@gmail.com>
2024-12-05 08:18:27 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "hemantp1999@gmail.com";
+var accountPassword = "423442";
+accountCity = "Varanasi";
+let accountState;
+
+// accountId = 2;
+accountEmail = "hp@gmail.com";
+accountPassword = "909090";
+accountCity = "Noida";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+prefer not to use var cause of block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert("Hello") // we are using nodejs not browser
+
+console.log(3+3);
+
+let name = "Jay"
+let age = 32
+let isLoggedIn = false
+
+// number
+// BigInt
+// Boolean => true/false
+// String => ""
+// null => standalone value
+// undefined => values is not given yet
+// Symbol => unique
+
+
+// Object
+
+console.log(typeof "abc");
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn)
+console.log(typeof null);// object
+console.log(typeof undefined); // undefined
